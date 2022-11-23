38e306ce1c791440723c1674788100ba4e1653c1
Kamrul Islam <kamrul_bd_islam@yahoo.com>
2022-11-24 01:22:01 +0900

    Added nav bar and basic design

--- api/controllers/login_controller.py
+++ api/controllers/login_controller.py
@@ -1,11 +1,6 @@
-import connexion
+# import connexion
 
-from templates.api.services.login_service import LoginService
+from api.services.login_service import LoginService
 
 def login_controller():
-
-    if connexion.request.method == "POST":
-        ls = LoginService()
-        print("connexion is working")
-
     return "working"
